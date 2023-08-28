009f09d7c692fa977d2c9940d62f5f05b1f84273
gluke0 <milanesigianluca@gmail.com>
2023-08-28 16:23:38 +0200

    logo and menu draft

--- src/components/header.vue
+++ src/components/header.vue
@@ -13,13 +13,38 @@ export default {
 </script>
 
 <template>
-
-   <div>
-   
-   </div>
+  <div class="container-lg">
+    <div class="d-flex justify-content-between logodiv">
+      <div>
+        <img src="../../public/img/header/logo.webp" alt="Studio Tecnico Pedrini Logo">
+      </div>
+      <div class="menudiv">
+        <ul class="d-flex">
+          <li class="ms-4">Chi Siamo</li>
+          <li class="ms-4">Servizi</li>
+          <li class="ms-4">Dove Siamo</li>
+          <li class="ms-4">Contatti</li>
+        </ul>
+      </div>
+    </div>
+  </div>
     
 </template>
 
 <style lang="scss">
+
+img{
+  width: 100%;
+}
+
+.logodiv{
+  width: 40%;
+}
+
+.menudiv{
+  width: 60%;
+}
+
+
 
 </style>
