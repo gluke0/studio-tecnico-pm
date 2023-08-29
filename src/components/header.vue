7cce57ed027f8b9508de7c9bbe82af0bcda2b2fc
gluke0 <milanesigianluca@gmail.com>
2023-08-29 16:33:54 +0200

    added link to menu

--- src/components/header.vue
+++ src/components/header.vue
@@ -17,10 +17,10 @@ export default {
     <div class="">
       <div class="menudiv">
         <ul class="d-flex justify-content-around">
-          <li class="ms-4">Chi Siamo</li>
-          <li class="ms-4">Servizi</li>
-          <li class="ms-4">Dove Siamo</li>
-          <li class="ms-4">Contatti</li>
+          <li class="ms-4"><a href="">Chi siamo</a></li>
+          <li class="ms-4"><a href="">Servizi</a></li>
+          <li class="ms-4"><a href="">Dove Siamo</a></li>
+          <li class="ms-4"><a href="">Contatti</a></li>
         </ul>
       </div>
       <div class="logodiv d-flex justify-content-center">
