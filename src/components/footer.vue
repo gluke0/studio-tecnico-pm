ebfa92f8237eae2a07f516619c3de2a9acfa65eb
gluke0 <milanesigianluca@gmail.com>
2023-08-30 17:12:34 +0200

    added footer draft

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -13,7 +13,34 @@ export default {
 </script>
 
 <template>
-    
+  <div class="container">
+    <div class="d-flex justify-content-between">
+      <div>
+        <ul class="">
+          <li class="ms-4"><a href="">Chi siamo</a></li>
+          <li class="ms-4"><a href="">Servizi</a></li>
+          <li class="ms-4"><a href="">Dove Siamo</a></li>
+          <li class="ms-4"><a href="">Contatti</a></li>
+        </ul>
+      </div>
+      <div>
+        <ul class="">
+          <li class="ms-4"><a href="">Chi siamo</a></li>
+          <li class="ms-4"><a href="">Servizi</a></li>
+          <li class="ms-4"><a href="">Dove Siamo</a></li>
+          <li class="ms-4"><a href="">Contatti</a></li>
+        </ul>
+      </div>
+      <div>
+        <ul class="">
+          <li class="ms-4"><a href="">Chi siamo</a></li>
+          <li class="ms-4"><a href="">Servizi</a></li>
+          <li class="ms-4"><a href="">Dove Siamo</a></li>
+          <li class="ms-4"><a href="">Contatti</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style lang="scss">
