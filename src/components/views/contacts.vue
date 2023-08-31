8cb6759819c90587f2122f7cb41bec59cdf787d9
gluke0 <milanesigianluca@gmail.com>
2023-08-31 16:54:56 +0200

    link modified

--- src/components/views/contacts.vue
+++ src/components/views/contacts.vue
@@ -18,20 +18,20 @@ export default {
     <div class="container my-5">
         <div class="d-flex justify-content-around">
             <div class="icons d-flex row text-center">
-                <a target="_blank" href="https://www.facebook.com/Studio%20tecnico%20Pedrini"><i class="fa-solid fa-location-dot"></i></a>
+                <a target="_blank" href=""><i class="fa-solid fa-location-dot"></i></a>
                 <div> Via Italia, 4 </div>
                 <div> Casale Cremasco - Vidolasco </div>
                 <div> 26010 Cremona</div>
                 
             </div>
             <div class="icons d-flex row text-center">
-                <a target="_blank" class="p-0" href="https://www.facebook.com/Studio%20tecnico%20Pedrini"><i class="fa-solid fa-phone"></i></a>
+                <a target="_blank" class="p-0" href=""><i class="fa-solid fa-phone"></i></a>
                 <span>+39 3494759312</span>
             </div>
         </div>
         <div class="d-flex justify-content-around mt-5">
             <div class="icons d-flex row text-center">
-                <a class="text-decoration-none" target="_blank" href="mailto:studiotecnicopedrini@gmail.com"><i class="fa-solid fa-envelope"></i></a>
+                <a class="text-decoration-none" href="mailto:studiotecnicopedrini@gmail.com"><i class="fa-solid fa-envelope"></i></a>
                 <a class="text-decoration-none text-dark" href="mailto:studiotecnicopedrini@gmail.com">studiotecnicopedrini@gmail.com</a>
             </div>
             <div class="icons d-flex row text-center">
