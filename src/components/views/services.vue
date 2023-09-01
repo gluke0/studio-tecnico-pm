fc428b2450742d05e62f478bc5270a1892bed46b
gluke0 <milanesigianluca@gmail.com>
2023-09-01 16:27:25 +0200

    width

--- src/components/views/services.vue
+++ src/components/views/services.vue
@@ -15,46 +15,53 @@ export default {
 
 <template>
    
-    <div class="container">
-        <div>
-            <h4 class="text-uppercase">ufficio tecnico</h4>
-            <span>
-                Offriamo servizi di progettazione 3D e 2D che abbracciano diversi settori, dal meccanico al design. La nostra competenza ci permette di realizzare progetti personalizzati in co-design con il cliente, coinvolgendolo attivamente in ogni fase di sviluppo fino al completamento del progetto.
-            </span>
-        </div>
-        <div>
-            <h4 class="text-uppercase">prototipi</h4>
-            <span>
-                Realizzazione fusioni prototipali grezze e/o lavorate e piccole serie per valutazioni tecnico-meccaniche del cliente
-            </span>
-        </div>
-        <div>
-            <h4 class="text-uppercase">prototipazione rapida</h4>
-            <span>
-                Grazie a questa tecnologia siamo in grado di realizzare prototipi senza il costo di attrezzature in modo che il cliente possa già vedere fisicamente l'oggetto e valutarlo senza incappare in costi stampo magari da rivedere
-            </span>
-        </div>
-        <div>
-            <h4 class="text-uppercase">stampi</h4>
-            <span>
-                Siamo in grado di gestire varie tipologie di stampo; dal polistirolo al pressofuso, dal modello in legno alla conchiglia in gravità. 
-            </span>
-        </div>
-        <div>
-            <h4 class="text-uppercase">reverse engineering</h4>
-            <span>
-                Ricostruzione file 3D a partire da digitalizzazione di oggetti con Scanner 3D 
-            </span>
-        </div>
-        <div>
-            <h4 class="text-uppercase">lavorazioni meccaniche di precisione</h4>
-            <span>
-                Eseguiamo lavorazioni meccaniche di precisione. La nostra esperienza e un importante Know-how ci rendono il partner ideale per qualsiasi realtà
-            </span>
+    <div class="container d-flex justify-content-center">
+        <div class="services">
+            <div>
+                <h4 class="text-uppercase">ufficio tecnico</h4>
+                <span class="single-service">
+                    Offriamo servizi di progettazione 3D e 2D che abbracciano diversi settori, dal meccanico al design. La nostra competenza ci permette di realizzare progetti personalizzati in co-design con il cliente, coinvolgendolo attivamente in ogni fase di sviluppo fino al completamento del progetto.
+                </span>
+            </div>
+            <div>
+                <h4 class="text-uppercase">prototipi</h4>
+                <span class="single-service">
+                    Realizzazione fusioni prototipali grezze e/o lavorate e piccole serie per valutazioni tecnico-meccaniche del cliente
+                </span>
+            </div>
+            <div>
+                <h4 class="text-uppercase">prototipazione rapida</h4>
+                <span class="single-service">
+                    Grazie a questa tecnologia siamo in grado di realizzare prototipi senza il costo di attrezzature in modo che il cliente possa già vedere fisicamente l'oggetto e valutarlo senza incappare in costi stampo magari da rivedere
+                </span>
+            </div>
+            <div>
+                <h4 class="text-uppercase">stampi</h4>
+                <span class="single-service">
+                    Siamo in grado di gestire varie tipologie di stampo; dal polistirolo al pressofuso, dal modello in legno alla conchiglia in gravità. 
+                </span>
+            </div>
+            <div>
+                <h4 class="text-uppercase">reverse engineering</h4>
+                <span class="single-service">
+                    Ricostruzione file 3D a partire da digitalizzazione di oggetti con Scanner 3D 
+                </span>
+            </div>
+            <div>
+                <h4 class="text-uppercase">lavorazioni meccaniche di precisione</h4>
+                <span class="single-service mt-5">
+                    Eseguiamo lavorazioni meccaniche di precisione. La nostra esperienza e un importante Know-how ci rendono il partner ideale per qualsiasi realtà
+                </span>
+            </div>
         </div>
     </div>
 </template>
 
 <style lang="scss">
 @import '../../style/main.scss';
+
+.services{
+    width: 75%;
+}
+
 </style>
