16c34835bd5f9c329e88fce5aa946a32e2b690b8
gluke0 <milanesigianluca@gmail.com>
2023-09-05 17:59:31 +0200

    progress

--- src/components/views/news.vue
+++ src/components/views/news.vue
@@ -14,12 +14,21 @@ export default {
 
 <template>
     
-   
+   <div class="container">
+
+    <div class="content">
+        here just news and sponsorhips
+    </div>
+
+   </div>
 
 </template>
 
 <style lang="scss">
 @import '../../style/main.scss';
 
+.content{
+    width: 75%;
+}
 
 </style>
