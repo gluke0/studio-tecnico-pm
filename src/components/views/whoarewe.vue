151c1163fc390f1102db7d5c5f49ff87f66a54d2
gluke0 <milanesigianluca@gmail.com>
2023-10-11 17:17:36 +0200

    layout text

--- src/components/views/whoarewe.vue
+++ src/components/views/whoarewe.vue
@@ -19,7 +19,11 @@ export default {
 
           Lo studio tecnico Pedrini offre servizi di consulenza e produzione meccanica per aziende di qualsiasi dimensione e settore. Grazie a decenni di esperienza e alla vasta rete di aziende meccaniche che fanno parte della nostra holding, siamo in grado di gestire progetti di qualsiasi complessità, fornendo prodotti finiti di alta qualità o consulenze personalizzate.
 
+          <hr>
+
           Siamo il partner ideale per trasformare le vostre idee in realtà. Offriamo una combinazione unica di know-how, professionalità e innovazione, che vi permetterà di migliorare l'efficienza dei vostri processi produttivi, ridurre i costi e aumentare la competitività. 
+
+          <hr>
 
           Per saperne di più sui nostri servizi e su come possiamo aiutarvi a crescere, contattateci per una consulenza gratuita. 
             
@@ -36,5 +40,9 @@ export default {
     line-height: 2;
     font-size: 1.1rem;
     font-style: italic;
+}
+
+hr{
+  width: 0.01rem;
 }
 </style>
