aac9482a166f66e798aa1e9db5852d55ceb77265
gluke0 <milanesigianluca@gmail.com>
2023-11-22 17:01:48 +0100

    changed boxes titles style

--- src/components/views/services.vue
+++ src/components/views/services.vue
@@ -18,37 +18,37 @@ export default {
     <div class="container d-flex justify-content-center">
         <div class="services">
             <div class="box-service mt-0">
-                <h4 class="text-uppercase">ufficio tecnico</h4>
+                <h4 class="text-uppercase ser-title">ufficio tecnico</h4>
                 <span class="single-service">
                     Offriamo servizi di progettazione 3D e 2D che abbracciano diversi settori, dal meccanico al design. La nostra competenza ci permette di realizzare progetti personalizzati in co-design con il cliente, coinvolgendolo attivamente in ogni fase di sviluppo fino al completamento del progetto.
                 </span>
             </div>
             <div class="box-service">
-                <h4 class="text-uppercase">prototipi</h4>
+                <h4 class="text-uppercase ser-title">prototipi</h4>
                 <span class="single-service">
                     Realizzazione fusioni prototipali grezze e/o lavorate e piccole serie per valutazioni tecnico-meccaniche del cliente
                 </span>
             </div>
             <div class="box-service">
-                <h4 class="text-uppercase">prototipazione rapida</h4>
+                <h4 class="text-uppercase ser-title">prototipazione rapida</h4>
                 <span class="single-service">
                     Grazie a questa tecnologia siamo in grado di realizzare prototipi senza il costo di attrezzature in modo che il cliente possa già vedere fisicamente l'oggetto e valutarlo senza incappare in costi stampo magari da rivedere
                 </span>
             </div>
             <div class="box-service">
-                <h4 class="text-uppercase">stampi</h4>
+                <h4 class="text-uppercase ser-title">stampi</h4>
                 <span class="single-service">
                     Siamo in grado di gestire varie tipologie di stampo; dal polistirolo al pressofuso, dal modello in legno alla conchiglia in gravità. 
                 </span>
             </div>
             <div class="box-service">
-                <h4 class="text-uppercase">reverse engineering</h4>
+                <h4 class="text-uppercase ser-title">reverse engineering</h4>
                 <span class="single-service">
                     Ricostruzione file 3D a partire da digitalizzazione di oggetti con Scanner 3D 
                 </span>
             </div>
             <div class="box-service">
-                <h4 class="text-uppercase">lavorazioni meccaniche di precisione</h4>
+                <h4 class="text-uppercase ser-title">lavorazioni meccaniche di precisione</h4>
                 <span class="single-service mt-5">
                     Eseguiamo lavorazioni meccaniche di precisione. La nostra esperienza e un importante Know-how ci rendono il partner ideale per qualsiasi realtà
                 </span>
@@ -70,5 +70,10 @@ export default {
     background-color: rgb(183, 179, 179);
 }
 
+.ser-title{
+    font-weight: bolder;
+    font-style: italic;
+    font-size: 2rem;
+}
 
 </style>
