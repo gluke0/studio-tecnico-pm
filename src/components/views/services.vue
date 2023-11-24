53f9ffa17ba661ac1e59097fb1776c0268b184d8
gluke0 <milanesigianluca@gmail.com>
2023-11-24 11:41:08 +0100

    img added to the page

--- src/components/views/services.vue
+++ src/components/views/services.vue
@@ -22,36 +22,54 @@ export default {
                 <span class="single-service">
                     Offriamo servizi di progettazione 3D e 2D che abbracciano diversi settori, dal meccanico al design. La nostra competenza ci permette di realizzare progetti personalizzati in co-design con il cliente, coinvolgendolo attivamente in ogni fase di sviluppo fino al completamento del progetto.
                 </span>
+                <div>
+                    <img src="/img/ufficio.webp/" alt="">
+                </div>
             </div>
             <div class="box-service">
                 <h4 class="text-uppercase ser-title">prototipi</h4>
                 <span class="single-service">
                     Realizzazione fusioni prototipali grezze e/o lavorate e piccole serie per valutazioni tecnico-meccaniche del cliente
                 </span>
+                <div>
+                    <img src="/img/prototipi.webp/" alt="">
+                </div>
             </div>
             <div class="box-service">
                 <h4 class="text-uppercase ser-title">prototipazione rapida</h4>
                 <span class="single-service">
                     Grazie a questa tecnologia siamo in grado di realizzare prototipi senza il costo di attrezzature in modo che il cliente possa già vedere fisicamente l'oggetto e valutarlo senza incappare in costi stampo magari da rivedere
                 </span>
+                <div>
+                    <img src="/img/prot-rapida.webp/" alt="">
+                </div>
             </div>
             <div class="box-service">
                 <h4 class="text-uppercase ser-title">stampi</h4>
                 <span class="single-service">
                     Siamo in grado di gestire varie tipologie di stampo; dal polistirolo al pressofuso, dal modello in legno alla conchiglia in gravità. 
                 </span>
+                <div>
+                    <img src="/img/stampi.webp/" alt="">
+                </div>
             </div>
             <div class="box-service">
                 <h4 class="text-uppercase ser-title">reverse engineering</h4>
                 <span class="single-service">
                     Ricostruzione file 3D a partire da digitalizzazione di oggetti con Scanner 3D 
                 </span>
+                <div>
+                    <img src="/img/reverse.webp/" alt="">
+                </div>
             </div>
             <div class="box-service">
                 <h4 class="text-uppercase ser-title">lavorazioni meccaniche di precisione</h4>
                 <span class="single-service mt-5">
                     Eseguiamo lavorazioni meccaniche di precisione. La nostra esperienza e un importante Know-how ci rendono il partner ideale per qualsiasi realtà
                 </span>
+                <div>
+                    <img src="/img/precision.webp/" alt="">
+                </div>
             </div>
         </div>
     </div>
