fdf7923b1df8a731aaf4a935b9d8db79a2bda55a
gluke0 <milanesigianluca@gmail.com>
2023-11-27 16:59:59 +0100

    align center text boxes

--- src/components/views/services.vue
+++ src/components/views/services.vue
@@ -24,7 +24,7 @@ export default {
                     <div class="d-flex justify-content-center align-items-center">
                         <img class="imgser" src="/img/ufficio.webp/" alt="">
                     </div>
-                    <span class="single-service">
+                    <span class="single-service d-flex align-items-center">
                         Offriamo servizi di progettazione 3D e 2D che abbracciano diversi settori, dal meccanico al design. La nostra competenza ci permette di realizzare progetti personalizzati in co-design con il cliente, coinvolgendolo attivamente in ogni fase di sviluppo fino al completamento del progetto.
                     </span>
                 </div>
@@ -33,7 +33,7 @@ export default {
             <div class="box-service">
                 <h4 class="text-uppercase ser-title">prototipi</h4>
                 <div class="d-flex justify-content-around wrap">
-                    <span class="single-service">
+                    <span class="single-service d-flex align-items-center">
                         Realizzazione fusioni prototipali grezze e/o lavorate e piccole serie per valutazioni tecnico-meccaniche del cliente
                     </span>
                     <div class="d-flex justify-content-center align-items-center">
@@ -48,7 +48,7 @@ export default {
                     <div class="d-flex justify-content-center align-items-centerr">
                         <img class="imgser" src="/img/prot-rapida.webp/" alt="">
                     </div>
-                    <span class="single-service">
+                    <span class="single-service d-flex align-items-center">
                         Grazie a questa tecnologia siamo in grado di realizzare prototipi senza il costo di attrezzature in modo che il cliente possa già vedere fisicamente l'oggetto e valutarlo senza incappare in costi stampo magari da rivedere
                     </span>
                 </div>
@@ -57,7 +57,7 @@ export default {
             <div class="box-service">
                 <h4 class="text-uppercase ser-title">stampi</h4>
                 <div class="d-flex justify-content-around wrap">
-                    <span class="single-service">
+                    <span class="single-service d-flex align-items-center">
                         Siamo in grado di gestire varie tipologie di stampo; dal polistirolo al pressofuso, dal modello in legno alla conchiglia in gravità. 
                     </span>
                     <div class="d-flex justify-content-center align-items-center">
@@ -72,7 +72,7 @@ export default {
                     <div class="d-flex justify-content-center align-items-center">
                         <img class="imgser" src="/img/reverse.webp/" alt="">
                     </div>
-                    <span class="single-service">
+                    <span class="single-service d-flex align-items-center">
                         Ricostruzione file 3D a partire da digitalizzazione di oggetti con Scanner 3D 
                     </span>
                 </div>
@@ -81,7 +81,7 @@ export default {
             <div class="box-service">
                 <h4 class="text-uppercase ser-title">lavorazioni meccaniche di precisione</h4>
                 <div class="d-flex justify-content-around wrap">
-                    <span class="single-service">
+                    <span class="single-service d-flex align-items-center">
                         Eseguiamo lavorazioni meccaniche di precisione. La nostra esperienza e un importante Know-how ci rendono il partner ideale per qualsiasi realtà
                     </span>
                     <div class="d-flex justify-content-center align-items-center">
