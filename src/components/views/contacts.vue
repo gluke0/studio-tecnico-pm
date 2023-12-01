dab672b2e7f735fc9d054450d1f6e208e6428ae1
gluke0 <milanesigianluca@gmail.com>
2023-12-01 17:57:26 +0100

    added start call to number (not tested yet)

--- src/components/views/contacts.vue
+++ src/components/views/contacts.vue
@@ -26,7 +26,7 @@ export default {
                 
             </div>
             <div class="icons d-flex row text-center">
-                <a target="_blank" class="p-0" href=""><i class="fa-solid fa-phone"></i></a>
+                <a target="_blank" class="p-0" href="tel:+393494759312"><i class="fa-solid fa-phone"></i></a>
                 <span> +39 3494759312 </span>
             </div>
         </div>
